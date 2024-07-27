83cafd46e5e7ea0ba21ac1e8b3d09a3631b3f030
BewellDental <165155886+Asglobalsofttech23@users.noreply.github.com>
2024-07-27 20:05:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -40,17 +40,17 @@ const upload = multer({ storage: storage });
 const uploadExcel = multer({storage: storageExcel});
 
 const db = mysql.createPool({
-    host: "127.0.0.1",
-    port: "3306",
-    user: "root",
-    password: "0210",
-    database: "school"
+    // host: "127.0.0.1",
+    // port: "3306",
+    // user: "root",
+    // password: "0210",
+    // database: "school"
 
-    // host:'193.203.184.74',
-    // port:"3306",
-    // user:'u534462265_exschool',
-    // password:'ASGlobal@12345',
-    // database:'u534462265_exschool'
+    host:'193.203.184.74',
+    port:"3306",
+    user:'u534462265_dreamschool',
+    password:'ASGlobal@12345',
+    database:'u534462265_dreamschool'
 
 });
 
